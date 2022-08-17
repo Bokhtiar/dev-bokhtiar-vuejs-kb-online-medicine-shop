607518cf72f534acc65713175f40e37e61568bd6
bokhtiar <bokhtiartoshar1@gmail.com>
2022-08-17 10:23:43 +0600

    vue router setup done

--- src/App.vue
+++ src/App.vue
@@ -1,111 +1,17 @@
 <template>
-  <section class="">
-    <!--to navbar start -->
-    <div class="container">
-      <div class="d-flex bd-highlight">
-        <div class="me-auto bd-highlight">Get 10% offer this month</div>
-        <div class="bd-highlight"> <span class="m-1">Order Track</span> </div>
-        <div class="bd-highlight"> <span class="m-1">My Account</span> </div>
-        <div class="bd-highlight"> <span class="m-1">Shopping cart(2)</span> </div>
-      </div>
-    </div>
-    <hr/>
-    <!--top navbar end -->
-
-    <!--start of navbar main part -->
-    <nav class="navbar navbar-expand-lg navbar-light">
-      <div class="container-fluid container">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav m-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdown"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Dropdown
-              </a>
-              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><hr class="dropdown-divider" /></li>
-                <li>
-                  <a class="dropdown-item" href="#">Something else here</a>
-                </li>
-              </ul>
-            </li>
-            <li class="nav-item">
-              <a
-                class="nav-link disabled"
-                href="#"
-                tabindex="-1"
-                aria-disabled="true"
-                >Disabled</a
-              >
-            </li>
-          </ul>
-          <div class="d-flex">
-            <ul class="navbar-nav m-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link" href=""
-                  ><span class="material-symbols-outlined">
-                    shopping_cart
-                  </span></a
-                >
-              </li>
-              <li class="nav-item">
-                <a href="" class="nav-link"
-                  ><span class="material-symbols-outlined"> search </span></a
-                >
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </nav>
-    <!--end of navbar main part -->
-
-    <!--banner start -->
-    <section>
-      <div class="banner-img">
-        <div class="container banner">
-          <h2 class="banner-title">Mono Watch</h2>
-          <p class="banner-text">Cool Example Of Creative Medical Tools</p>
-          <a class="btn custom-btn" href="">Shop Now</a>
-        </div>
-      </div>
-    </section>
-    <!--banner end -->
-  </section>
+  <div id="app">
+    <router-view/>
+  </div>
 </template>
 
 <script>
-import("../src/components/User/Assets/style.css");
+// import("../src/components/User/Assets/style.css");
 
 export default {
   name: "App",
-  components: {},
+  components: {
+
+  },
 };
 </script>
 
---
+++ src/components/User/home.vue
@@ -0,0 +1,11 @@
+<template>
+    <section>
+        hi bokhtiar
+        <p class="text-danger">asdfasdfasdf </p>
+    </section>
+</template>
+<script>
+export default {
+    
+}
+</script>
